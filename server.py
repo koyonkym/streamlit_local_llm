04d454999317ca4a02b35d9fcd0f604062a2c024
Koyo Nakayama <taiyou.003.084@gmail.com>
2024-05-05 22:18:08 +0900

    replaced langserve with flask for http server implementation

--- server.py
+++ server.py
@@ -1,29 +1,98 @@
-from fastapi import FastAPI
-from langserve import add_routes
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from transformers import pipeline
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import render_template
+import logging
+import os
+import sys
 
-
-app = FastAPI(
-    title="LangChain Server",
-    version="1.0",
-    description="A simple api server using Langchain's Runnable interfaces",
-)
 
 model = AutoModelForCausalLM.from_pretrained("distilbert/distilgpt2")
 model.load_adapter("distilgpt2-instruct")
 tokenizer = AutoTokenizer.from_pretrained("distilbert/distilgpt2")
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100)
-llm = HuggingFacePipeline(pipeline=pipe)
-add_routes(
-    app,
-    llm,
-    path="/instruction",
-)
 
-if __name__ == "__main__":
-    import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+SERVICE_HOST = os.getenv('SERVER_HOST', '0.0.0.0')
+SERVER_PORT = os.getenv('SERVER_PORT', 8080)
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(
+        logging.Formatter(
+            '%(name)s [%(asctime)s] [%(levelname)s] %(message)s'))
+    logger.addHandler(handler)
+    return logger
+
+
+logger = get_logger('llm-service')
+
+app = Flask(__name__)
+
+
+@app.get("/healthcheck")
+def readiness_probe():
+    return "I'm ready!"
+
+
+@app.post("/instruction")
+def instruction():
+    '''
+    Main handler for input data sent by Snowflake.
+    '''
+    message = request.json
+    logger.debug(f'Received request: {message}')
+
+    if message is None or not message['data']:
+        logger.info('Received empty message')
+        return {}
+
+    # input format:
+    #   {"data": [
+    #     [row_index, column_1_value, column_2_value, ...],
+    #     ...
+    #   ]}
+    input_rows = message['data']
+    logger.info(f'Received {len(input_rows)} rows')
+
+    # output format:
+    #   {"data": [
+    #     [row_index, column_1_value, column_2_value, ...}],
+    #     ...
+    #   ]}
+    output_rows = [[row[0], get_llm_response(row[1])] for row in input_rows]
+    logger.info(f'Produced {len(output_rows)} rows')
+
+    response = make_response({"data": output_rows})
+    response.headers['Content-type'] = 'application/json'
+    logger.debug(f'Sending response: {response.json}')
+    return response
+
+
+@app.route("/ui", methods=["GET", "POST"])
+def ui():
+    '''
+    Main handler for providing a web UI.
+    '''
+    if request.method == "POST":
+        # getting input in HTML form
+        input_text = request.form.get("input")
+        # display input and output
+        return render_template("basic_ui.html",
+            llm_input=input_text,
+            llm_reponse=get_llm_response(input_text))
+    return render_template("basic_ui.html")
+
+
+def get_llm_response(input):
+    return pipe(input, return_full_text=False)[0]['generated_text']
+
+if __name__ == '__main__':
+    app.run(host=SERVICE_HOST, port=SERVER_PORT)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
-from langserve import RemoteRunnable
 import pandas as pd
 import numpy as np
+import requests
 
 
 st.title('Uber pickups in NYC')
@@ -32,16 +31,21 @@ hist_dict = {"hour": list(range(0, 24)), "pickups": np.histogram(data[DATE_COLUM
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-instruction = RemoteRunnable("http://localhost:8000/instruction/")
+prompt = f"### Question: Explore the Uber pickups data for New York City, showcasing the number of pickups per hour. The dataset includes hourly counts of pickups throughout the day. Provide insights into the trends and patterns observed in the data. Data: {str(hist_dict)}\n ### Answer: "
 
-prompt_template = PromptTemplate.from_template(
-    "### Question: Explore the Uber pickups data for New York City, showcasing the number of pickups per hour. The dataset includes hourly counts of pickups throughout the day. Provide insights into the trends and patterns observed in the data. Data: {data}\n ### Answer: "
-)
-
-chain = prompt_template | instruction
-
-def generate_response(data):
-    st.info(chain.invoke({"data": data})[len(prompt_template.format(data=data)):])
+def generate_response(prompt):
+    url = 'http://localhost:8080/instruction'
+    input_rows = [[0, prompt]]
+    data = {
+        'data': input_rows,
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print('Error:', response.status_code)
+    message = response.json()
+    st.info(message['data'][0][1])
     st.warning("This project focuses solely on verifying the integration between Streamlit and the local Large Language Model (LLM). Please note that generated texts may lack coherence or relevance.", icon="⚠️")
 
-generate_response(str(hist_dict))
+generate_response(prompt)
